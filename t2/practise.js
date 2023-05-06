3dc56b469702e0b3174e267328cfa77973bf4323
hemanth <applicationcomputer257@gmail.com>
2023-05-06 10:18:17 +0530

    changes

--- t2/practise.js
+++ t2/practise.js
@@ -14,3 +14,35 @@ console.log(base)
 const absolutePath=path.resolve(__dirname,"subFolder","test.txt")
 
 console.log(absolutePath)
+
+// sync fs
+
+const fs=require('fs')
+
+// read file
+const first =fs.readFileSync('./t2/first.txt','utf-8')
+console.log(first)
+// write file
+fs.writeFileSync('./t2/zoro.txt',"mosh head")
+
+// async fs
+
+// readfile async
+const second =fs.readFile('./t2/second.txt','utf-8',(err,res)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(res)
+    }
+})
+
+//  write async
+fs.writeFile("./t2/zoro.txt","four sword",(err,res)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(res)
+    }
+})
